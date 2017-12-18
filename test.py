e54468ab5bc1408e96107538c414c627c83e2e8d
tamasKiss <sauriaster@gmail.com>
2017-12-18 21:34:50 +0100

    Downloading hi-res version of pictures, only test with had coded gallery, code need to clean later

--- test.py
+++ test.py
@@ -3,23 +3,39 @@ import requests
 
 def download(result):
     download = requests.get(result[0]['href'])
-    d_soup = BeautifulSoup(download.raw, 'html.parser')
-    return d_soup
+    return download
 
-def write_file(download):
-    with open("test_image.png", 'wb') as fd:
+def write_file(download, file_name):
+    with open(file_name, 'wb') as fd:
             for chunk in download.iter_content(chunk_size=4096):
                 fd.write(chunk)
 
+def fetch_html(page_url):
+    da_page = requests.get(page_url)
+    mysoup = BeautifulSoup(da_page.text, 'html.parser')
+    return mysoup
+    
 
-da_page = requests.get("https://pridark.deviantart.com/gallery/")
-soup = BeautifulSoup(da_page.text, 'html.parser')
-result = soup.find_all('a', href='https://pridark.deviantart.com/art/Comm-Blue-and-Orange-720560563')
-link = result[0]['href']
-print("RESULT : ", link)
+page_url = 'https://lulemt.deviantart.com/gallery/'
+soup = fetch_html(page_url)
+outer_div = soup.find("div", class_='folderview-art')
+result = outer_div.find_all("a", class_="torpedo-thumb-link")
+links = [item['href'] for item in result]
+
+INDEX_OF_HI_RES = 0
+INDEX_OF_NAME = -1
+for link in links:
+    print("FETCHED LINK: ", link)    
+    url = fetch_html(link)
+    out_div = url.find("div", class_='dev-view-deviation')
+    res = [i['src'] for i in out_div.find_all("img", class_='dev-content-full')]
+    split_name = res[INDEX_OF_HI_RES].split('/')
+    print("SAVED AS: {} \n".format(split_name[INDEX_OF_NAME]))
+    
+    write_file(requests.get(res[INDEX_OF_HI_RES]), split_name[INDEX_OF_NAME])
+
+
 print("-" * 50)
-#download = download(result)
-#write_file(download)
 print("OK")
 
 
